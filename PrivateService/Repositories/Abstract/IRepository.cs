060faa8cb263052592fc692810fc931c1a845d50
yarik111475 <yarik111475@mail.ru>
2018-02-04 20:21:14 +0400

    Project changed 04.02.2018 at 20:21

--- PrivateService/Repositories/Abstract/IRepository.cs
+++ PrivateService/Repositories/Abstract/IRepository.cs
@@ -7,5 +7,15 @@ using PrivateService.Models;
 
 namespace PrivateService.Repositories.Abstract {
     public interface IRepository {
+        #region READ
+        Pacient[] ReadPaciens();
+        Region[] ReadRegions();
+        City[] ReadCities();
+        Doctor[] ReadDoctors();
+        Speciality[] ReadSpecialities();
+        Clinic[] ReadClinics();
+        Appointment[] ReadAppointments();
+        DoctorsToClinics[] ReadDoctorsToClinics();
+        #endregion
     }
 }
