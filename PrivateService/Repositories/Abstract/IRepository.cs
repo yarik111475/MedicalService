b57f6324a19116278e21f43766c048afb72a08ab
yarik111475 <yarik111475@mail.ru>
2018-02-04 20:11:37 +0400

    Project changed 04.02.2018 at 20:11

---
+++ PrivateService/Repositories/Abstract/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using PrivateService.Models;
+
+namespace PrivateService.Repositories.Abstract {
+    public interface IRepository {
+    }
+}
