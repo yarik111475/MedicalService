150325ba2762ea07fedb46a0f7078255df2820a8
yarik111475 <yarik111475@mail.ru>
2018-02-04 20:48:40 +0400

    Create repository interface 04.02.2018 at 20:21

--- PrivateService/Repositories/Abstract/IRepository.cs
+++ PrivateService/Repositories/Abstract/IRepository.cs
@@ -7,6 +7,17 @@ using PrivateService.Models;
 
 namespace PrivateService.Repositories.Abstract {
     public interface IRepository {
+        #region CREATE
+        int CreatePacient(Pacient pacient);
+        void CreateRegion(Region region);
+        void CreateCity(City city);
+        void CreateDoctor(Doctor doctor);
+        void CreateSpeciality(Speciality speciality);
+        void CreateClinic(Clinic clinic);
+        void CreateAppointment(Appointment appointment);
+        void CreateDoctorsToClinics(DoctorsToClinics doctorstoclinics);
+        #endregion
+
         #region READ
         Pacient[] ReadPaciens();
         Region[] ReadRegions();
@@ -16,6 +27,39 @@ namespace PrivateService.Repositories.Abstract {
         Clinic[] ReadClinics();
         Appointment[] ReadAppointments();
         DoctorsToClinics[] ReadDoctorsToClinics();
+        #endregion
+
+        #region UPDATE
+        void UpdatePacients(Pacient pacient);
+        void UpdateRegions(Region region);
+        void UpdateCities(City city);
+        void UpdateDoctors(Doctor doctor);
+        void UpdateSpecialities(Speciality speciality);
+        void UpdateClinics(Clinic clinic);
+        void UpdateAppointments(Appointment appointment);
+        void UpdateDoctorsToClinics(DoctorsToClinics doctorstoclinics);
+        #endregion
+
+        #region DELETEBYID
+        void DeletePacientById(int pacientId);
+        void DeleteRegionByRegionId(int regionId);
+        void DeleteCityByCityId(int cityId);
+        void DeleteDoctorByDoctorId(int doctorId);
+        void DeleteSpecialityBySpecialityId(int specialityId);
+        void DeleteClinicByClinicId(int clinicId);
+        void DeleteAppointmentByAppointmentId(int appointmentId);
+        void DeleteDoctorsAndClinicsByDoctorsAndClinicsId(int doctorsandclinicsId);
+        #endregion
+
+        #region READBYID
+        Pacient ReadPacientByPacientId(int pacientId);
+        Region ReadRegionByRegionId(int regionId);
+        City ReadCityByCityId(int cityId);
+        Doctor ReadDoctorByDoctorId(int doctorId);
+        Speciality ReadSpecialityBySpecialityId(int specialityId);
+        Clinic ReadClinicByClinicId(int clinicId);
+        Appointment ReadAppointmentByAppointmentId(int appointmentId);
+        DoctorsToClinics ReadDoctorsToClinicsByDoctorsToClinicsId(int doctorstoclinicsId);
         #endregion
     }
 }
